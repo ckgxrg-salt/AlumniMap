5b60adb905ffc8e399a29939768d9863e7446968
ckgxrg <ckgxrg@ckgxrg.io>
2025-05-16 02:59:53 +0800

    Add wasm rust target

--- flake.nix
+++ flake.nix
@@ -1,34 +1,40 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, rust-overlay, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
+        overlays = [ (import rust-overlay) ];
       };
     in
     {
       devShells.${system}.default = pkgs.mkShell rec {
         name = "alumnimap";
 
-        buildInputs = with pkgs; [
-          cargo
-          rustc
+        nativeBuildInputs = with pkgs; [
+          (rust-bin.stable.latest.default.override {
+            targets = [ "wasm32-unknown-unknown" ];
+          })
+
           clippy
           rustfmt
           deadnix
           nixfmt-rfc-style
 
-          # Backend
           loco
           sea-orm-cli
 
-          # Frontend
           trunk
+          llvmPackages.bintools
+        ];
+
+        buildInputs = with pkgs; [
           openssl
           pkg-config
           libxkbcommon
