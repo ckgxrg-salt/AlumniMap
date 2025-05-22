9ac40d0bc749792c329ad7b020b089c5a52cf63b
ckgxrg <ckgxrg@ckgxrg.io>
2025-05-23 04:05:09 +0800

    Backend: Add profiles query route

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -1,8 +1,8 @@
 use actix_web::{get, web, HttpResponse};
-use sea_orm::EntityTrait;
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
 
 use crate::server::AppState;
-use entity::university;
+use entity::{profile, university};
 
 #[get("/ping")]
 pub async fn ping() -> HttpResponse {
@@ -12,6 +12,18 @@ pub async fn ping() -> HttpResponse {
 #[get("/universities")]
 pub async fn universities(state: web::Data<AppState>) -> HttpResponse {
     let list = university::Entity::find().all(&state.db).await;
+    match list {
+        Ok(result) => HttpResponse::Ok().json(serde_json::to_string(&result).unwrap_or_default()),
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
+}
+
+#[get("/profiles/{uni_id}")]
+pub async fn profiles(state: web::Data<AppState>, path: web::Path<i32>) -> HttpResponse {
+    let list = profile::Entity::find()
+        .filter(profile::Column::UniversityId.eq(path.into_inner()))
+        .all(&state.db)
+        .await;
     match list {
         Ok(result) => HttpResponse::Ok().json(serde_json::to_string(&result).unwrap_or_default()),
         Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -6,6 +6,7 @@ pub fn setup(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api")
             .service(handlers::ping)
-            .service(handlers::universities),
+            .service(handlers::universities)
+            .service(handlers::profiles),
     );
 }
