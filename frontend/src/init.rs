8825d03abacaf72e2607ae1958c570e05f1231bd
ckgxrg <ckgxrg@ckgxrg.io>
2025-05-25 17:37:35 +0800

    Frontend: Add nerd font

--- frontend/src/init.rs
+++ frontend/src/init.rs
@@ -1,23 +1,34 @@
 //! Initialise the app
 
-use egui::{FontData, FontDefinitions, FontFamily};
+use egui::{FontData, FontDefinitions};
 
 use crate::widgets::map::WorldMap;
 
 pub fn init_font(ctx: &egui::Context) {
     let mut fonts = FontDefinitions::default();
     fonts.font_data.insert(
-        "my_font".to_owned(),
+        "notosans-sc".to_owned(),
         std::sync::Arc::new(FontData::from_static(include_bytes!(
             "../assets/NotoSansSC.ttf"
+        ))),
+    );
+    fonts.font_data.insert(
+        "notosans-nf".to_owned(),
+        std::sync::Arc::new(FontData::from_static(include_bytes!(
+            "../assets/NotoSansNF.ttf"
         ))),
     );
 
     fonts
         .families
-        .get_mut(&FontFamily::Proportional)
-        .unwrap()
-        .insert(0, "my_font".to_owned());
+        .entry(egui::FontFamily::Proportional)
+        .or_default()
+        .insert(0, "notosans-sc".to_owned());
+    fonts
+        .families
+        .entry(egui::FontFamily::Proportional)
+        .or_default()
+        .insert(0, "notosans-nf".to_owned());
 
     ctx.set_fonts(fonts);
 }
