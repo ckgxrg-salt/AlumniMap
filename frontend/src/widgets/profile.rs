43b8735154c81b3760a763e3eeea2fb7e7169ab1
ckgxrg <ckgxrg@ckgxrg.io>
2025-05-25 17:37:46 +0800

    ProfileCard: Sort out layout

--- frontend/src/widgets/profile.rs
+++ frontend/src/widgets/profile.rs
@@ -79,23 +79,61 @@ impl ProfileCard {
     pub fn render(&mut self, ui: &mut egui::Ui) {
         self.fetch_data(ui.ctx());
         ui.horizontal(|ui| {
-            ui.image(format!(
+            let image = egui::Image::new(format!(
                 "http://127.0.0.1:8080/static/avatars/{}",
                 self.inner.avatar
-            ));
+            ))
+            .shrink_to_fit();
+            ui.add(image);
             ui.separator();
             ui.vertical(|ui| {
-                ui.label(format!(
-                    "{} {}          {}届",
-                    self.inner.name_primary.clone(),
-                    self.inner.name_supplementary.clone().unwrap_or_default(),
-                    self.inner.class_of
-                ));
+                ui.horizontal(|ui| {
+                    ui.with_layout(egui::Layout::left_to_right(egui::Align::LEFT), |ui| {
+                        ui.label(self.inner.name_primary.clone());
+                        ui.label(self.inner.name_supplementary.clone().unwrap_or_default());
+                    });
+                    ui.with_layout(egui::Layout::right_to_left(egui::Align::RIGHT), |ui| {
+                        ui.label(format!("{}届", self.inner.class_of));
+                    });
+                });
                 //ui.label(self.university_name.clone());
-                ui.label(self.inner.major.clone().unwrap_or_default());
+                ui.horizontal(|ui| {
+                    ui.with_layout(egui::Layout::left_to_right(egui::Align::LEFT), |ui| {
+                        ui.label(self.inner.bio.clone().unwrap_or_default());
+                    });
+                    ui.with_layout(egui::Layout::right_to_left(egui::Align::RIGHT), |ui| {
+                        ui.label(self.inner.major.clone().unwrap_or_default());
+                    });
+                });
                 ui.separator();
-                ui.label(self.inner.bio.clone().unwrap_or_default());
+                self.render_contacts(ui);
             });
         });
+    }
+
+    /// Draws the collapsible contacts section
+    fn render_contacts(&self, ui: &mut egui::Ui) {
+        egui::CollapsingHeader::new("󰛋".to_string())
+            .id_salt(self.inner.id)
+            .show(ui, |ui| {
+                ui.horizontal(|ui| {
+                    ui.with_layout(egui::Layout::top_down(egui::Align::TOP), |ui| {
+                        if let Some(text) = self.inner.wechat.clone() {
+                            ui.label(format!("󰘑 {text}"));
+                        }
+                        if let Some(text) = self.inner.qq.clone() {
+                            ui.label(format!("󰘅 {text}"));
+                        }
+                    });
+                    ui.with_layout(egui::Layout::top_down(egui::Align::TOP), |ui| {
+                        if let Some(text) = self.inner.email.clone() {
+                            ui.label(format!("󰇮 {text}"));
+                        }
+                        if let Some(text) = self.inner.matrix.clone() {
+                            ui.label(format!("󰘨 {text}"));
+                        }
+                    });
+                });
+            });
     }
 }
