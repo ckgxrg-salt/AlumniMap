ac2131d91dda558612a8258de960668bbbae3952
ckgxrg <ckgxrg@ckgxrg.io>
2025-05-29 01:17:51 +0800

    ProfileCard: Adjust layout

--- frontend/src/widgets/card.rs
+++ frontend/src/widgets/card.rs
@@ -20,18 +20,15 @@ pub fn render(model: &profile::Model, ui: &mut egui::Ui) {
         ui.separator();
         ui.vertical(|ui| {
             ui.horizontal(|ui| {
-                ui.with_layout(egui::Layout::left_to_right(egui::Align::LEFT), |ui| {
-                    ui.label(&model.name_primary);
-                    ui.label(extract_string(model.name_supplementary.as_ref()));
-                });
+                ui.label(&model.name_primary);
+                ui.label(extract_string(model.name_supplementary.as_ref()));
                 ui.with_layout(egui::Layout::right_to_left(egui::Align::RIGHT), |ui| {
-                    ui.label(format!("{}届", model.class_of));
+                    ui.label(format!("{}", model.class_of));
                 });
             });
             ui.horizontal(|ui| {
-                ui.with_layout(egui::Layout::left_to_right(egui::Align::LEFT), |ui| {
-                    ui.label(extract_string(model.bio.as_ref()));
-                });
+                ui.label(extract_string(model.bio.as_ref()));
+                ui.add_space(100.0);
                 ui.with_layout(egui::Layout::right_to_left(egui::Align::RIGHT), |ui| {
                     ui.label(extract_string(model.major.as_ref()));
                 });
@@ -48,23 +45,20 @@ fn render_contacts(model: &profile::Model, ui: &mut egui::Ui) {
     egui::CollapsingHeader::new("󰛋".to_string())
         .id_salt(model.id)
         .show(ui, |ui| {
-            ui.horizontal(|ui| {
-                ui.with_layout(egui::Layout::top_down(egui::Align::TOP), |ui| {
-                    if let Some(text) = &model.wechat {
-                        ui.label(format!("󰘑 {text}"));
-                    }
-                    if let Some(text) = &model.qq {
-                        ui.label(format!("󰘅 {text}"));
-                    }
-                });
-                ui.with_layout(egui::Layout::top_down(egui::Align::TOP), |ui| {
-                    if let Some(text) = &model.email {
-                        ui.label(format!("󰇮 {text}"));
-                    }
-                    if let Some(text) = &model.matrix {
-                        ui.label(format!("󰘨 {text}"));
-                    }
-                });
+            egui::Grid::new(model.id).show(ui, |ui| {
+                if let Some(text) = &model.wechat {
+                    ui.label(format!("󰘑 {text}"));
+                }
+                if let Some(text) = &model.qq {
+                    ui.label(format!("󰘅 {text}"));
+                }
+                ui.end_row();
+                if let Some(text) = &model.email {
+                    ui.label(format!("󰇮 {text}"));
+                }
+                if let Some(text) = &model.matrix {
+                    ui.label(format!("󰘨 {text}"));
+                }
             });
         });
 }
